f237fecde25bcdf5041edeec02f1c768729e1193
Tampoka <goal.thanks@gmail.com>
2021-09-21 14:37:49 +0900

    delete unnecessary buttons

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -14,15 +14,6 @@ import "./../../App.css"
             <Star selected={value>2} onRatingChangeHandler={onRatingChangeHandler}/>
             <Star selected={value>3} onRatingChangeHandler={onRatingChangeHandler}/>
             <Star selected={value>4} onRatingChangeHandler={onRatingChangeHandler}/>
-            {/*<button onClick={()=>setValue(1)}>1</button>*/}
-            {/*<Star selected={value>1}/>*/}
-            {/*<button onClick={()=>setValue(2)}>2</button>*/}
-            {/*<Star selected={value>2}/>*/}
-            {/*<button onClick={()=>setValue(3)}>3</button>*/}
-            {/*<Star selected={value>3}/>*/}
-            {/*<button onClick={()=>setValue(4)}>4</button>*/}
-            {/*<Star selected={value>4}/>*/}
-            {/*<button onClick={()=>setValue(5)}>5</button>*/}
         </div>
     );
 }
