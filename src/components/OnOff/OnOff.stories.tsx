2006ca39fe13f23fbb074f77982a3370b3a76895
Tampoka <goal.thanks@gmail.com>
2021-09-23 21:22:50 +0900

    created story for OnOff2

---
+++ src/components/OnOff/OnOff.stories.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {OnOff2} from "../OnOff2/OnOff2";
+
+export default {
+    title: 'OnOff2',
+    component: OnOff2,
+    argTypes: {},
+} as ComponentMeta<typeof OnOff2>;
+
+const Template: ComponentStory<typeof OnOff2> = (args) => <OnOff2 {...args} />;
+
+export const OnOffSwitching = () => {
+    const [on, setOn] = useState<boolean>(false)
+    return <OnOff2 on={on} onClick={setOn}/>
+}
+export const OnMode = Template.bind({});
+OnMode.args = {
+    on: true,
+};
+export const OffMode = Template.bind({});
+OffMode.args = {
+    on: false,
+};
