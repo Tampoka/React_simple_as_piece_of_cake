9eab6ebc8a539d2b3e20e5e887280cfa039444cd
Tampoka <goal.thanks@gmail.com>
2021-09-24 14:59:57 +0900

    created story for OnOff2Uncontrolled

---
+++ src/components/OnOff2Uncontrolled/OnOff2Unconterolled.stories.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {ComponentMeta} from '@storybook/react';
+import {OnOff2Uncontrolled} from "./OnOff2Uncontrolled";
+
+export default {
+    title: 'OnOff2Uncontrolled',
+    component: OnOff2Uncontrolled,
+    argTypes: {},
+} as ComponentMeta<typeof OnOff2Uncontrolled>;
+
+export const OnOffSwitching = () => {
+    const [on, setOn] = useState<boolean>(false)
+    return <OnOff2Uncontrolled  onChange={setOn}/>
+}
