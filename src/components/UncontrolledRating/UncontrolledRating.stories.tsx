babb0e3ae6af9de7c11641ce798cdf288ee23399
Tampoka <goal.thanks@gmail.com>
2021-09-24 20:03:34 +0900

    created action for onChange

--- src/components/UncontrolledRating/UncontrolledRating.stories.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {UncontrolledRating} from "./UncontrolledRating";
-import {Rating} from "../Rating/Rating";
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -11,34 +11,12 @@ export default {
     },
 } as ComponentMeta<typeof UncontrolledRating>;
 
-const Template: ComponentStory<typeof Rating> = (args) => <Rating {...args} />;
+const Template: ComponentStory<typeof UncontrolledRating> = (args) => <UncontrolledRating {...args} />;
+
+const callback=action("rating changed inside component")
 
 export const RatingChanging = () => {
-    return <UncontrolledRating defaultValue={4}/>
+    return <UncontrolledRating defaultValue={4} onChange={callback}/>
 }
 
 
-export const EmptyRating = Template.bind({});
-EmptyRating.args = {
-    value: 0,
-};
-export const Rating1 = Template.bind({});
-Rating1.args = {
-    value: 1,
-};
-export const Rating2 = Template.bind({});
-Rating2.args = {
-    value: 2,
-};
-export const Rating3 = Template.bind({});
-Rating3.args = {
-    value: 3,
-};
-export const Rating4 = Template.bind({});
-Rating4.args = {
-    value: 4,
-};
-export const Rating5 = Template.bind({});
-Rating5.args = {
-    value: 5,
-};
--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -4,6 +4,7 @@ import {RatingValueType} from "../Rating/Rating";
 
 type RatingPropsType={
     defaultValue?:RatingValueType
+    onChange:(value:RatingValueType)=>void
 }
 export function UncontrolledRating(props:RatingPropsType) {
     // console.log('UncontrolledRating is rendering')
@@ -11,11 +12,11 @@ export function UncontrolledRating(props:RatingPropsType) {
 
     return (
         <div className={"ratingUncontrolled"}>
-            <Star selected={value > 0} setValue={()=>{setValue(1)}}/>
-            <Star selected={value > 1} setValue={()=>{setValue(2)}}/>
-            <Star selected={value > 2} setValue={()=>{setValue(3)}}/>
-            <Star selected={value > 3} setValue={()=>{setValue(4)}}/>
-            <Star selected={value > 4} setValue={()=>{setValue(5)}}/>
+            <Star selected={value > 0} setValue={()=>{setValue(1);props.onChange(1)}}/>
+            <Star selected={value > 1} setValue={()=>{setValue(2);props.onChange(2)}}/>
+            <Star selected={value > 2} setValue={()=>{setValue(3);props.onChange(3)}}/>
+            <Star selected={value > 3} setValue={()=>{setValue(4);props.onChange(4)}}/>
+            <Star selected={value > 4} setValue={()=>{setValue(5);props.onChange(5)}}/>
         </div>
     );
 }
