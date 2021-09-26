e5c0f6613a5d5181cb00c74c75f203494d3b1191
Tampoka <goal.thanks@gmail.com>
2021-09-26 16:24:09 +0900

    added hover

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -3,15 +3,21 @@ import s from "./StarRating.module.css"
 
 export const StarRating = () => {
     const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
     return (
         <div className={s.starRating}>
             {[...Array(5)].map((star, index) => {
                 index += 1
                 return (
-                    <button type="button"
-                            key={index}
-                            onClick={() => setRating(index)}
-                            style={{color: index <= rating ? 'darkred' : 'grey'}}>
+                    <button
+                        type="button"
+                        key={index}
+                        onClick={() => setRating(index)}
+                        onMouseEnter={() => setHover(index)}
+                        onMouseLeave={() => setHover(rating)}
+                        style={{color: index <= (rating||hover) ? 'darkred' : 'grey'}}
+                        // className={index<=(hover||rating)?'on':'off'}
+                    >
                         <span>&#9733;</span>
                     </button>
                 )
