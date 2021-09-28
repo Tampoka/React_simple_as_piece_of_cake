2c91098c3c1a47e30f795479c040304b211404e5
Tampoka <goal.thanks@gmail.com>
2021-09-28 14:25:36 +0900

    added callbackProps

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,31 +1,29 @@
 import React, {useState} from 'react';
-import {ComponentMeta, ComponentStory, Story} from '@storybook/react';
+import {Story} from '@storybook/react';
 import {Accordion, AccordionPropsType} from "./Accordion";
 import {action} from "@storybook/addon-actions";
 
-export default {
-    title: 'Accordion',
-    component: Accordion,
-    argTypes: {},
-} as ComponentMeta<typeof Accordion>;
 
 const callback = action("accordion mode change event fired")
 const onClickCallback = action("some item was clicked")
 
 const Template: Story<AccordionPropsType> = (args) => <Accordion {...args} />;
 
-export const AccordionCollapsedMode = Template.bind({})
-AccordionCollapsedMode.args={
-    titleValue:"Menu",
-    collapsed:true,
+const callbackProps={
     onClick:onClickCallback
 }
+export const AccordionCollapsedMode = Template.bind({})
+AccordionCollapsedMode.args={
+    ...onClickCallback,
+    titleValue:"Menu",
+    collapsed:true,
+}
 
-export const AccordionUncollapsedMode = Template.bind({});
-AccordionUncollapsedMode.args = {
+export const AccordionOpenMode = Template.bind({});
+AccordionOpenMode.args = {
+    ...onClickCallback,
     collapsed: false,
     titleValue:"Users",
-    onClick:onClickCallback
 };
 
 export const ModeChanging=()=>{
