46da12188d153e5faa4f6316b2b19d49db2f9859
Tampoka <goal.thanks@gmail.com>
2021-09-30 13:45:00 +0900

    added inputId

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -7,16 +7,22 @@ export default {
 
 export const UncontrolledInput = () => <input/>
 export const UncontrolledInputWithTrackedValue = () => {
-    const [value, setValue]=useState('')
-    return <><input onChange={(e)=>{
-    const actualValue=e.currentTarget.value
-    setValue(actualValue)}
+    const [value, setValue] = useState('')
+    return <><input onChange={(e) => {
+        const actualValue = e.currentTarget.value
+        setValue(actualValue)
+    }
     }/>---{value}--</>
 }
 
 export const GetValueOfUncontrolledInputByButtonPress = () => {
-    const [value, setValue]=useState('')
-    return <><input /><button>save</button>-actual value: {value}</>
+    const [value, setValue] = useState('')
+    return <><input id={"inputId"}/>
+        <button onClick={() => {
+            setValue("yo")
+        }}>save
+        </button>
+        -actual value: {value} </>
 }
 export const ControlledInputWithFixedValue = () => <input value={"google"}/>
 
