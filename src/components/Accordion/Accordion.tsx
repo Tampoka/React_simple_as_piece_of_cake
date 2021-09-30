cf75ccdcdfb6b8c86cf8179554aa9888b93a2c80
Tampoka <goal.thanks@gmail.com>
2021-09-30 12:59:07 +0900

    added story for input

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -9,38 +9,47 @@ export type AccordionPropsType = {
      * Callback that is called when any item clicked
      * @param collapsed
      */
-    onClick:(collapsed:boolean)=>void
-
-    collapsed:boolean
+    onClick: (value: any) => void
+    onChange: () => void
+    collapsed: boolean
+    items: ItemType[]
 }
- export function Accordion(props: AccordionPropsType) {
+type ItemType = {
+    title: string
+    value: any
+}
+
+export function Accordion(props: AccordionPropsType) {
     // console.log('Accordion is rendering')
 
     return <div>
-        <AccordionTitle title={props.titleValue} onClick={props.onClick} collapsed={props.collapsed}/>
-        {!props.collapsed && <AccordionBody/>}
+        <AccordionTitle title={props.titleValue}
+                        onChange={props.onChange}/>
+        {!props.collapsed && <AccordionBody items={props.items} onClick={props.onClick}/>}
     </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
-    onClick:(collapsed:boolean)=>void
-    collapsed:boolean
+    onChange: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     // console.log('AccordionTitle is rendering')
-    return <h2 onClick={()=>{props.onClick(!props.collapsed)}}>{props.title}</h2>
+    return <h3 onClick={(e) => props.onChange()}>{props.title}</h3>
 
 
 }
 
-function AccordionBody() {
+type AccordionBodyPropsType = {
+    items: ItemType[]
+    onClick: (value: any) => void
+}
+
+function AccordionBody(props: AccordionBodyPropsType) {
     // console.log('AccordionBody is rendering')
     return <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        {props.items.map((i, index) => <li onClick={() => {props.onClick(i.value)}} key={index}>{i.title}</li>)}
     </ul>
 
 }
