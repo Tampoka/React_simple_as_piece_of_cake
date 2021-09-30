7400bb7b08eebfd7ae22a989bbc301ad1e9d062b
Tampoka <goal.thanks@gmail.com>
2021-09-30 13:25:40 +0900

    added UncontrolledInputWithTrackedValue

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,37 +1,18 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React, {useState} from 'react';
 
-import { Button } from './Button';
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+    title: 'Input/input'
+}
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+export const UncontrolledInput = () => <input/>
+export const UncontrolledInputWithTrackedValue = () => {
+    const [value, setValue]=useState('')
+    return <><input onChange={(e)=>{
+    const actualValue=e.currentTarget.value
+    setValue(actualValue)}
+    }/>---{value}--</>
+}
+export const ControlledInputWithFixedValue = () => <input value={"google"}/>
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
