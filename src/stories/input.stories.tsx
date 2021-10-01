18693a7b473b6d65bb0e379a32518fbb2e876ce0
Tampoka <goal.thanks@gmail.com>
2021-10-01 09:07:12 +0900

    refactored  callback onChange

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -9,7 +9,7 @@ export const UncontrolledInput = () => <input/>
 
 export const UncontrolledInputWithTrackedValue = () => {
     const [value, setValue] = useState('')
-    const onChange=(e:ChangeEvent<HTMLInputElement>) => {
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         const actualValue = e.currentTarget.value
         setValue(actualValue)
     }
@@ -18,21 +18,25 @@ export const UncontrolledInputWithTrackedValue = () => {
 
 export const GetValueOfUncontrolledInputByButtonPress = () => {
     const [value, setValue] = useState('')
-    const inputRef=useRef<HTMLInputElement>(null)
-    const save=() => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
         // const el=document.getElementById("inputId") as HTMLInputElement
         const el = inputRef.current as HTMLInputElement
         setValue(el.value)
     }
     return <><input ref={inputRef} id={"inputId"}/>
-        <button onClick={save}>save</button>-actual value: {value} </>
+        <button onClick={save}>save</button>
+        -actual value: {value} </>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={"google"}/>
 
-export const ControlledInput=()=>{
-    const [parentValue, setParentValue]=useState("")
-    return <input value={parentValue} onChange={(e)=>{setParentValue(e.currentTarget.value)}}/>
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    const onChange = (e:ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.value)
+
+    }
+    return <input value={parentValue} onChange={onChange}/>
 }
 
 // export const ControlledCheckbox=()=>{
