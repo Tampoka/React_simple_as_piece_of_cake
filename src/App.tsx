562203709746106c7bc6dd60a61b4695610599c5
Tampoka <goal.thanks@gmail.com>
2021-10-01 18:53:15 +0900

    finished logic? for custom select

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,21 @@ import {UncontrolledAccordion} from "./components/UncontrolledAccordion/Uncontro
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 import {OnOff2Uncontrolled} from "./components/OnOff2Uncontrolled/OnOff2Uncontrolled";
 import {StarRating} from "./components/StarRating/StarRating";
+import {CustomSelect} from "./components/CustomSelect/CustomSelect";
 
 function App() {
     let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     let [collapsed, setCollapsed] = useState<boolean>(false)
     let [on, setOn] = useState<boolean>(false)
     const items = [{title: "Bob", value: 4}, {title: "John", value: 7}, {title: "Kate", value: 5}]
-    const onClick = (value: any) => {
-        alert(`user with id ${value} should be happy `)
-    }
+    const onClick = (value: any) => alert(`user with id ${value} should be happy `)
+    const [selectValue, setSelectValue]=useState<string>("Please select from ...")
+
     return (
         <div className={"App"}>
             <PageTitle title={"This as App component"}/>
             <PageTitle title={"My friends"}/>
+            <CustomSelect onChange={setSelectValue} items={items} selectValue={selectValue}/>
             <OnOff/>
             <OnOff2 onClick={setOn}
                     on={on}/>
--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, {MouseEventHandler, useState} from "react";
 
-type CustomSelectPropsType={
-    defaultValue?:string
-    onChange:(value:any)=>void
-    items:ItemType[]
+type CustomSelectPropsType = {
+    selectValue: string
+    onChange: (selectValue: string) => void
+    items: ItemType[]
 }
 
 type ItemType = {
@@ -11,11 +11,21 @@ type ItemType = {
     value: any
 }
 
-export function CustomSelect(props:CustomSelectPropsType) {
+export function CustomSelect(props: CustomSelectPropsType) {
+    const [collapsed, setCollapsed] = useState<boolean>(true)
+/*    const onItemClickedHandler=(i)=>{
+        props.onChange(i.title)
+        setCollapsed(true)
+    }*/
     return (
         <div>
-            <div>{}</div>
-            {props.items.map(i=><div>{i.title}</div>)}
+            <div onClick={() => setCollapsed(!collapsed)}>{props.selectValue}</div>
+            {!collapsed && props.items
+                .filter(i=>i.title!==props.selectValue)
+                .map(i => <div key={i.value} onClick={()=> {
+                    props.onChange(i.title);
+                    setCollapsed(true)
+                }}>{i.title}</div>)}
         </div>
     )
 }
