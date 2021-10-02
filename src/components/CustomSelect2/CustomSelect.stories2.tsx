0324de0f7896143fc3d921ecf0f54a792e41c31f
Tampoka <goal.thanks@gmail.com>
2021-10-02 17:49:58 +0900

    created stories for  CustomSelect2

--- src/components/CustomSelect2/CustomSelect.stories2.tsx
+++ src/components/CustomSelect2/CustomSelect.stories2.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import {ComponentMeta, Story} from '@storybook/react';
 import {action} from "@storybook/addon-actions";
-import {CustomSelect2, CustomSelectPropsType} from "./CustomSelect2";
+import {CustomSelect2, CustomSelect2PropsType} from "./CustomSelect2";
 
 export default {
     title: 'components/CustomSelect2',
     component: CustomSelect2,
     argTypes: {},
 } as ComponentMeta<typeof CustomSelect2>;
+/*
 
 const callback = action("Select value change event fired")
 
-const Template: Story<CustomSelectPropsType> = (args) => <CustomSelect2 {...args} />;
+const Template: Story<CustomSelect2PropsType> = (args) => <CustomSelect2 {...args} />;
 
 
 export const SelectWithoutValue = Template.bind({})
 SelectWithoutValue.args = {
-    selectValue: "Please choose from...",
+    value: "Please choose from...",
     onChange: callback,
     items: [{title: "Bob", value: 4}, {title: "John", value: 7}, {title: "Kate", value: 5}]
 }
+*/
 
-
+export const CustomSelect = () => <CustomSelect2 onChange={action("Value changed")}
+                                                 value={"1"}
+                                                 items={[{value: "1", title: "Movie"},
+                                                     {value: "2", title: "Book"},
+                                                     {value: "3", title: "Walk"},]}/>
