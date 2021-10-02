7290ecde8360166ee544c381451b50ec38de2e97
Tampoka <goal.thanks@gmail.com>
2021-10-02 17:10:04 +0900

    added additional div for custom_list

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -21,13 +21,14 @@ export function CustomSelect(props: CustomSelectPropsType) {
     return (
         <div className={s.select_wrapper}>
             <div onClick={() => setCollapsed(!collapsed)} className={s.select_trigger}>{props.selectValue}<span className={s.select_triangle}>&#x25BC;</span></div>
-            {!collapsed && props.items
+            <div className={s.select_list}>{!collapsed && props.items
                 .filter(i => i.title !== props.selectValue)
                 .map((i) => {
                     return <div key={i.value} onClick={() => {
                         onItemClickedHandler(i.title)
                     }} className={s.select_item}>{i.title}</div>
                 })}
+            </div>
         </div>
     )
 }
