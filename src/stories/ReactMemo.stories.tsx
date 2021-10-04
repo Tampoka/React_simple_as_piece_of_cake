efb9ca62631327a186a3c5c20518a6f4ae11c81e
Tampoka <goal.thanks@gmail.com>
2021-10-04 22:07:16 +0900

    finished story

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -14,20 +14,21 @@ const UsersSecret = (props: { users: Array<string> }) => {
         {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
-const Users=React.memo(UsersSecret)
+const Users = React.memo(UsersSecret)
 
 export const Example1 = () => {
     console.log("Example")
     const [count, setCount] = useState(0)
     const [users, setUsers] = useState(['John', 'Bob', 'Kate', 'Ann'])
 
-        const addUser=()=>{
+    const addUser = () => {
         // users.push('Alex'+new Date().getTime())
-            const newUsers=[...users,'Alex'+new Date().getTime()]
-            setUsers(newUsers)
-        }
+        const newUsers = [...users, 'Alex' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
     return <>
-        <button onClick={()=>setCount(count+1)}>+</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
         <button onClick={addUser}>add user</button>
         <NewMessagesCounter count={count}/>
         <Users users={users}/>
