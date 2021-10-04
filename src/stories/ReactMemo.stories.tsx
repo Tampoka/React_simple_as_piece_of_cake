6bd5a29cde13ffddac45826144b4bb2f3ecc8a87
Tampoka <goal.thanks@gmail.com>
2021-10-04 21:36:03 +0900

    added key

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -11,7 +11,7 @@ const NewMessagesCounter = (props: { count: number }) => {
 const UsersSecret = (props: { users: Array<string> }) => {
     console.log("USERS")
     return <div>
-        {props.users.map((u, i) => <div>{u}</div>)}
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
 const Users=React.memo(UsersSecret)
