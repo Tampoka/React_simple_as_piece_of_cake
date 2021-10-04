fc7e8bd94fc5bc1f7c6b7ca374cc907d74fd3843
Tampoka <goal.thanks@gmail.com>
2021-10-04 12:55:32 +0900

    -created StateType, - in switch return making StateCopy

--- src/components/UncontrolledAccordion/Reducer.tsx
+++ src/components/UncontrolledAccordion/Reducer.tsx
@@ -1,15 +1,22 @@
 type ActionType = {
     type: string
 }
+
+export type StateType={
+    collapsed:boolean
+}
 export const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"
-export const reducer = (state: boolean, action: ActionType) => {
+export const reducer = (state: StateType, action: ActionType):StateType => {
     /*    console.log("reducer started ")
         console.log(state)
         console.log(action)
         console.log("reducer ended")*/
     switch (action.type) {
         case TOGGLE_CONSTANT:
-            return !state
+            return {
+                ...state,
+            collapsed:!state.collapsed
+    }
         default:
             throw new Error("Bad action type")
     }
