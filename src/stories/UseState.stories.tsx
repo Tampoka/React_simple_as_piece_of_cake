0b31f15e6865694ffc706d41101479a6f080c845
Tampoka <goal.thanks@gmail.com>
2021-10-16 20:54:29 +0900

    added useMemo

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,12 +1,20 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 
 export default {
     title: "useState Demo"
 }
 
+function generateData (){
+//    complex calculation
+    console.log('generateData')
+    return 4449303923232321
+}
 export const Example1 = () => {
     console.log("Example")
-    const [count, setCount] = useState(0)//[0, function(newValue)]
+
+    const initValue=useMemo(generateData,[])
+
+    const [count, setCount] = useState(initValue)//[0, function(newValue)]
 
     return <>
         <button onClick={() => setCount(count + 1)}>+</button>
